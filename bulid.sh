921776712118e5100e7dc80804bda0fe9a928de4
黄大卫 <huangdawei@meituan.com>
2017-08-03 09:02:50 +0800

    init the rpo

---
+++ bulid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "============ hexo clean begin ============"
+hexo clean
+echo "============ hexo generate begin ============"
+hexo generate
+echo "============ hexo deploy begin ============"
+hexo deploy
+
+echo "============ DONE ============"
---
+++ source/_posts/hexo-github-io-Travis.md
@@ -0,0 +1,5 @@
+---
+title: hexo+github.io+Travis
+date: 2017-08-03 00:46:57
+tags:
+---
